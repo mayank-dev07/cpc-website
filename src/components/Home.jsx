0acf7c8f10a1a1bbf6102d1b2ec0eeb58ebab7b2
mayank-dev07 <devmayank.hts@gmail.com>
2024-05-07 00:38:42 +0530

    terminal edited

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,11 +7,7 @@ const Home = () => {
   return (
     <>
       <div className="h-full bg-cover w-full md:w-1/2 flex justify-between items-center">
-        {/* <img src="cyber.png"></img> */}
         <div className="w-full h-full">
-          {/* <div className="w-full flex justify-center items-center text-[#0EC9AC] text-2xl md:text-3xl lg:text-5xl py-4">
-            Terminal
-          </div> */}
           <div
             className=" w-full h-[347px]
            flex flex-col"
@@ -20,18 +16,14 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className=" h-full w-full md:w-1/2 mt-5 md:mt-0">
-        <div className="text-2xl md:text-3xl lg:text-5xl text-center text-[#0EC9AC] flex justify-center item-center py-6 md: w-full">
+      <div className=" h-full w-full md:w-1/2 mt-8 md:mt-0">
+        <div className="text-2xl md:text-3xl lg:text-5xl text-center text-[#0EC9AC] flex justify-center item-center pt-8 md:py-6 md: w-full">
           Cyber&nbsp;Peace&nbsp;
           <ReactTyped
             strings={["Security", "Cell"]}
             typeSpeed={70}
             backSpeed={60}
             showCursor={true}
-            //               .dollar-cursor {
-            //   cursor: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"><text x="0" y="12" font-size="12">$</text></svg>'), auto;
-            // }
-
             loop
           />
         </div>
