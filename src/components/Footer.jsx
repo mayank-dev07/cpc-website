c375e0288a4a5c24450b4d02248f064b6e546cc8
mayank-dev07 <devmayank.hts@gmail.com>
2024-05-09 03:43:46 +0530

    issue solved

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Instagram, Globe, Twitter } from "lucide-react";
 const Footer = () => {
   return (
     <div className="w-full mt-auto flex flex-col md:flex-row z-20 h-full py-4 text-cyan-500 justify-center md:justify-between items-center gap-4 w-full font-medium  px-12">
-      <div className=" w-full flex justify-center md:justify-start">
+      <div className=" w-full flex justify-center md:justify-start text-cyan-500">
         $~Cyber&nbsp;Peace&nbsp;Center
       </div>
       <div className="flex justify-center md:justify-end md:w-max ">
