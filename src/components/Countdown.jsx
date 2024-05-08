071672991cee5a6d63f175195582f9d0527e9c58
mayank-dev07 <devmayank.hts@gmail.com>
2024-05-09 03:37:18 +0530

    issue solved

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -39,32 +39,32 @@ const Countdown = ({ targetDate }) => {
         <div className="flex justify-center gap-2 md:gap-8 h-[0px]">
           <div className="flex flex-col items-center justify-center w-100 px-2 py-2 h-[50px] gap-5 font-bold">
             DAYS{" "}
-            <div className="border-2 p-4 w-[65px] text-center text-2xl font-bold bg-gray-800 border-gray-800 rounded-md shadow-md shadow-[#0EC9AC] text-[#0EC9AC]">
+            <div className="border-2 p-4 w-[65px] text-center text-2xl font-bold bg-gray-800 border-gray-800 rounded-md shadow-md shadow-cyan-500 text-cyan-500">
               {days}
             </div>
           </div>
           <div className="flex flex-col items-center justify-center w-100 px-2 py-2 h-[50px] gap-5 font-bold uppercase ">
             hours
-            <div className="border-2 p-4 w-[65px] text-center text-2xl font-bold bg-gray-800 border-gray-800 rounded-md shadow-md shadow-[#0EC9AC] text-[#0EC9AC]">
+            <div className="border-2 p-4 w-[65px] text-center text-2xl font-bold bg-gray-800 border-gray-800 rounded-md shadow-md shadow-cyan-500 text-cyan-500">
               {hours}
             </div>
           </div>
           <div className="flex flex-col items-center justify-center w-100 px-2 py-2 h-[50px] gap-5 font-bold uppercase ">
             minutes
-            <div className="border-2 p-4 w-[65px] text-center text-2xl font-bold bg-gray-800 border-gray-800 rounded-md shadow-md shadow-[#0EC9AC] text-[#0EC9AC]">
+            <div className="border-2 p-4 w-[65px] text-center text-2xl font-bold bg-gray-800 border-gray-800 rounded-md shadow-md shadow-cyan-500 text-cyan-500">
               {minutes}
             </div>
           </div>
           <div className="flex flex-col items-center justify-center w-100 px-2 py-2 h-[50px] gap-5 font-bold uppercase ">
             seconds
-            <div className="border-2 p-4 w-[65px] text-center text-2xl font-bold bg-gray-800 border-gray-800 rounded-md shadow-md shadow-[#0EC9AC] text-[#0EC9AC]">
+            <div className="border-2 p-4 w-[65px] text-center text-2xl font-bold bg-gray-800 border-gray-800 rounded-md shadow-md shadow-cyan-500 text-cyan-500">
               {seconds}
             </div>
           </div>
         </div>
       ) : (
         <div className="w-full h-full flex justify-center  ">
-          <div className=" flex justify-center items-center px-6 py-3 font-bold bg-gray-800 border-gray-800 rounded-md shadow-md shadow-[#0EC9AC] text-[#0EC9AC]">
+          <div className=" flex justify-center items-center px-6 py-3 font-bold bg-gray-800 border-gray-800 rounded-md shadow-md shadow-cyan-500 text-cyan-500">
             <p>EVENT IS GOING 0N ... </p>
           </div>
         </div>
