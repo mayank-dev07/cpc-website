bc11d8dd67da274a134d476d2af6d0a9124bb7f6
Akshat Verma <112319520+Akshat162001@users.noreply.github.com>
2024-09-21 15:28:53 +0530

    Changes in Newsletter,Our Team, Timeline

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -4,7 +4,7 @@ const Members = () => {
   return (
     <>
       <div className="w-full flex flex-col justify-center items-center gap-4 pt-12">
-        <p className=" text-cyan-500 text-2xl ">Our Members</p>
+        <p className=" text-cyan-500 text-xl ">Our Members</p>
         <p className="text-4xl ">Meet Our Team</p>
       </div>
       <div className="flex flex-wrap flex-row w-full justify-center items-center px-0 md:px-8 lg:px-12 gap-8 lg:gap-12">
@@ -13,7 +13,9 @@ const Members = () => {
             <img src="/security.png" alt="" width={"50px"} />
           </div>
           <p className="text-center text-xl  text-cyan-500 pt-4 ">
-            Network Security
+          Kernel Security & Dark Web
+           <br />
+          <p className="text-xl text-white font-bold">Suryansh Deshwal</p>
           </p>
         </div>{" "}
         <div className="h-full md:min-h-[180px] w-10/12 sm:w-1/4 md:w-1/5 lg:w-1/6 bg-slate-800 rounded-lg p-5 flex flex-col  ">
@@ -21,7 +23,9 @@ const Members = () => {
             <img src="/security.png" alt="" width={"50px"} />
           </div>
           <p className="text-center text-xl  text-cyan-500 pt-4 ">
-            Network Security
+          Web Application Security
+          <br />
+          <p className="text-xl text-white font-bold">Yatharth Singh</p>
           </p>
         </div>{" "}
         <div className="h-full md:min-h-[180px] w-10/12 sm:w-1/4 md:w-1/5 lg:w-1/6 bg-slate-800 rounded-lg p-5 flex flex-col  ">
@@ -29,7 +33,10 @@ const Members = () => {
             <img src="/security.png" alt="" width={"50px"} />
           </div>
           <p className="text-center text-xl  text-cyan-500 pt-4 ">
-            Network Security
+          Digital Forensics 
+          <br />
+          <br />
+          <p className="text-xl text-white font-bold ">Rohit Gangwar</p>
           </p>
         </div>{" "}
         <div className="h-full md:min-h-[180px] w-10/12 sm:w-1/4 md:w-1/5 lg:w-1/6 bg-slate-800 rounded-lg p-5 flex flex-col  ">
@@ -37,7 +44,9 @@ const Members = () => {
             <img src="/security.png" alt="" width={"50px"} />
           </div>
           <p className="text-center text-xl  text-cyan-500 pt-4 ">
-            Network Security
+          Digital Forensics & Network Security 
+          <br />
+          <p className="text-xl text-white font-bold">Vijay Yadav</p>
           </p>
         </div>
         <div className="h-full md:min-h-[180px] w-10/12 sm:w-1/4 md:w-1/5 lg:w-1/6 bg-slate-800 rounded-lg p-5 flex flex-col  ">
@@ -45,7 +54,10 @@ const Members = () => {
             <img src="/database.png" alt="" width={"50px"} />
           </div>
           <p className="text-center text-xl  text-cyan-500 pt-4 ">
-            Database Security
+          Web Application & Cloud Security 
+          <br />
+          <p className="text-xl text-white font-bold">Aryan Sharma</p>
+
           </p>
         </div>{" "}
         <div className="h-full md:min-h-[180px] w-10/12 sm:w-1/4 md:w-1/5 lg:w-1/6 bg-slate-800 rounded-lg p-5 flex flex-col  ">
@@ -53,17 +65,13 @@ const Members = () => {
             <img src="/globe.png" alt="" width={"50px"} />
           </div>
           <p className="text-center text-xl  text-cyan-500 pt-4 ">
-            Cloud Security
+          Reversing & Malware Analysis
+          <br />
+          <p className="text-xl text-white font-bold">Indu Shekhar Pandey</p>
           </p>
+          
         </div>{" "}
-        <div className="h-full md:min-h-[180px] w-10/12 sm:w-1/4 md:w-1/5 lg:w-1/6 bg-slate-800 rounded-lg p-5 flex flex-col  ">
-          <div className="w-full flex justify-center ">
-            <img src="/security.png" alt="" width={"50px"} />
-          </div>
-          <p className="text-center text-xl  text-cyan-500 pt-4 ">
-            Network Security
-          </p>
-        </div>
+        
       </div>
     </>
   );
